25b0e66e3326795ca3a642499624d2f1e00866c7
CrazyGF2021 <81232608+CrazyGF2021@users.noreply.github.com>
2021-03-23 14:20:55 +0100

    Update settings.py

--- TA_Schulen/settings.py
+++ TA_Schulen/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gnat5d3ata&1@-(f*$f^oiw0=t2!^h&afq%8z2v4d47&1^nz30'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
