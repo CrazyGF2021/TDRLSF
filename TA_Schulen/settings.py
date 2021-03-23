bca0173fd63652fba2df2b846ef7c7df0594d88e
CrazyGF2021 <c.lau@westbus.de>
2021-03-23 13:11:12 +0100

    sett

--- TA_Schulen/settings.py
+++ TA_Schulen/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+#STATIC_ROOT= os.path.join(BASE_DIR,'static/')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'mysite_login'
