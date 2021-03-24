de39d7c48de6104a7e089a0c4a1b618dcac51472
CrazyGF2021 <81232608+CrazyGF2021@users.noreply.github.com>
2021-03-24 14:01:18 +0100

    Delete Fahrten directory

--- Fahrten/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Fahrten, Wochentag
-
-# Register your models here.
-admin.site.register(Fahrten)
-admin.site.register(Wochentag)
--- Fahrten/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from Fahrten.models import Fahrten
-
-class FahrtenDatumFilter(django_filters.FilterSet):
-    class Meta:
-        model = Fahrten
-        fields = ['gueltig_ab', 'gueltig_bis',]
